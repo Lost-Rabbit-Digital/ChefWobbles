b8803176ef7e7f3722b2b16fa71349ba85c94e2c
Boden McHale <bodenkaikarden@gmail.com>
2025-07-25 18:12:33 -0400

    Switching to perspective camera from ortho

--- godot_project/Scripts/OrthogonalCamera.gd
+++ godot_project/Scripts/OrthogonalCamera.gd
@@ -9,8 +9,8 @@ extends Node3D
 
 @export var camera_change_rate: float = 5.0
 
-@export var minimum_camera_size: float = 5
-@export var maximum_camera_size: float = 45
+@export var minimum_arm_distance: float = 5
+@export var maximum_arm_distance: float = 45
 
 
 @onready var spring_arm = $SpringArm3D
@@ -23,15 +23,15 @@ func _physics_process(delta):
 	
 	# Always clamp the camera
 	rotation_degrees.x = clamp(rotation_degrees.x, camera_angle_vertical, camera_angle_vertical)
-	ortho_camera.size = clamp(ortho_camera.size, minimum_camera_size, maximum_camera_size)
+	spring_arm.spring_length = clamp(spring_arm.spring_length, minimum_arm_distance, maximum_arm_distance)
 	
 	if Input.is_action_pressed("rotate_camera_left"):
 		rotation_degrees.y -= camera_change_rate
 	elif Input.is_action_pressed("rotate_camera_right"):
 		rotation_degrees.y += camera_change_rate
 	elif Input.is_action_just_pressed("camera_zoom_in"):
-		if (ortho_camera.size - camera_change_rate) > minimum_camera_size:
-			ortho_camera.size -= camera_change_rate
+		if (spring_arm.spring_length - camera_change_rate) > minimum_arm_distance:
+			spring_arm.spring_length -= camera_change_rate
 	elif Input.is_action_just_pressed("camera_zoom_out"):
-		if (ortho_camera.size + camera_change_rate) < maximum_camera_size:
-			ortho_camera.size += camera_change_rate
+		if (spring_arm.spring_length + camera_change_rate) < maximum_arm_distance:
+			spring_arm.spring_length += camera_change_rate
--- godot_project/Scripts/PlayerControlCamera.gd
+++ godot_project/Scripts/PlayerControlCamera.gd
@@ -8,6 +8,11 @@ extends Node3D
 
 @onready var spring_arm = $SpringArm3D
 
+@export var camera_change_rate: float = 1.0
+
+@export var minimum_arm_distance: float = 2
+@export var maximum_arm_distance: float = 45
+
 var mouse_lock = false # is mouse locked
 
 func _physics_process(delta):
@@ -27,6 +32,13 @@ func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		mouse_lock = true
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if Input.is_action_just_pressed("camera_zoom_in"):
+		if (spring_arm.spring_length - camera_change_rate) > minimum_arm_distance:
+			spring_arm.spring_length -= camera_change_rate
+	elif Input.is_action_just_pressed("camera_zoom_out"):
+		if (spring_arm.spring_length + camera_change_rate) < maximum_arm_distance:
+			spring_arm.spring_length += camera_change_rate
 	
 	#rotate camera
 	if event is InputEventMouseMotion and mouse_lock:
